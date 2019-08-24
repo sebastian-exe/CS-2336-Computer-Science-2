cfe3cf87bb2f899f73856bce6d5142fee14a1d57
Sebastian Moreno <46652667+sebastian-exe@users.noreply.github.com>
2019-08-24 18:30:07 -0500

    hw 2 description

---
+++ homeworks/hw2.java
@@ -0,0 +1,5 @@
+/*Write a program that prompts the user to enter
+the distance to drive,
+the fuel efficiency of the car in miles per gallon,
+and the price per gallon,
+and displays the cost of the trip.*/
