231183512b4e51ae759b43adde90fa1dd978c6d8
Sebastian Moreno <46652667+sebastian-exe@users.noreply.github.com>
2019-08-26 07:22:46 -0500

    initial commit

---
+++ homeworks/hw1pt4.java
@@ -0,0 +1,6 @@
+/*Write a program that prompts the user to enter a Social Security Number in the format DDD-DD-DDDD, where D is a digit. Your program should check whether the input is valid.
+ */
+
+
+
+
