63823669d99b271d129bbb05b79a86f52807d95e
Sebastian Moreno <46652667+sebastian-exe@users.noreply.github.com>
2019-09-09 23:17:34 -0500

    Update HW2_P2.java

--- homeworks/HW2_P2.java
+++ homeworks/HW2_P2.java
@@ -8,17 +8,28 @@ The problem is to print out the alphabet until a desired
 letter.
 */
 /* Write your Design (How to solve the problem):
-I plan on converting the character into ASCII values 
-and then using that ASCII value to loop through the
-alphabet until the desired letter is reached.
+I plan on setting up a flag to test if the character
+is an upper case or lower case letter. Once that has
+been decided, I have a character initialized for loop
+to print out the values up to the character
 */
 		//write your code here
-        int castAscii = ch;
-        int start = 'a';
-        for(int i = start; i <= castAscii; i++){
-            System.out.print((char)start);
-            System.out.print(" ");
-            start++;
+        //testing flag to see if the char is upper or lower
+        boolean test = Character.isUpperCase(ch);
+        if(test == false){
+            //for loop for lower case
+            for(char i = 'a'; i <= ch; i++){
+                System.out.print(i);
+                System.out.print(" ");
+                
+                }
+        }
+        else if(test == true){
+            //for loop for upper case
+            for(char j = 'A'; j <= ch; j++){
+                System.out.print(j);
+                System.out.print(" ");
+            }
         }
 	}
 }
